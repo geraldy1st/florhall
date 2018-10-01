497d6d264dd305e52e559f7c544737caa729776e
geraldy1st <geraldy.leondas@gmail.com>
2018-10-01 16:11:00 +0200

    menu js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-const menuBtn = document.querySelector(".btn-menu");
+// const menuBtn = document.querySelector(".btn-menu");
 
+const btnMenu = document.getElementById("btnMenu");
 const line1 = document.querySelectorAll(".btn-line")[0];
 const line2 = document.querySelectorAll(".btn-line")[1];
 const line3 = document.querySelectorAll(".btn-line")[2];
@@ -7,15 +8,13 @@ const line3 = document.querySelectorAll(".btn-line")[2];
 console.log(line1, line2, line3);
 
 // addEventListener;
-menuBtn.addEventListener("mouseover", menuOver);
-menuBtn.addEventListener("mouseleave", menuOut);
-menuBtn.addEventListener("click", menuShow);
+btnMenu.addEventListener("mouseover", menuOver);
+btnMenu.addEventListener("mouseleave", menuOut);
+btnMenu.addEventListener("click", menuShow);
 
 function menuOver() {
-  line1.style.width = "35px";
-  //   line2.style.width = "27px";
-  //   line2.style.marginLeft = "8px";
-  //   line3.style.width = "0px";
+  line2.classList.toggle("btn-line2-hover");
+  console.log("test");
 }
 function menuOut() {
   line2.style.width = "35px";
@@ -39,3 +38,9 @@ function menuShow() {
   background-color: #111;
 }
 */
+
+$(document).ready(function() {
+  $("#btnMenu").click(function() {
+    $(".menu").toggleClass("menuOn");
+  });
+});
